1913e2c2270edb7b9543d8b46c09114157f68a99
DaeHyunKang <dhkang@sudo.co.kr>
2019-05-22 16:06:23 +0900

    Command Class추가

---
+++ MyTools/MyTools/TempLibrary/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MyTools.TempLibrary
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        Action<object> _Execute;
+        Func<object, bool> _CanExecute;
+
+        public Command(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
